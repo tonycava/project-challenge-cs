a197d4f03d17d080fe0a5cda3e886829f56b366c
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-19 11:32:13 +0200

    Hero class refactor

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿using Game.Engine;
+using Game.Engine;
 
 public class Project : Board
 {
   public static void Main()
   {
-    int[] cordOfChar = new[] {0, 0};
-    DisplayGame(array2Db, cordOfChar);
-    GameLoop.Update(cordOfChar);
+    // int[] cordOfChar = new[] {0, 0};
+    // DisplayGame(array2Db, cordOfChar);
+    // GameLoop.Update(cordOfChar);
+    CreateCharacter.SetupCharacter();
   }
 }
+
+
--- System/createCharacter.cs
+++ System/createCharacter.cs
@@ -5,8 +5,6 @@
         public static void SetupCharacter()
         {
             Hero newHero = new Hero();
-            Console.WriteLine("Please enter your character name:");
-            newHero.HeroName = Console.ReadLine();
 
             Console.WriteLine("Now choose your character's class");
             Console.WriteLine("Warrior......1");
@@ -20,15 +18,15 @@
                 switch (classChosen)
                 {
                     case "1":
-                        newHero.HeroClass = "Warrior";
+                        newHero.Class = "Warrior";
                         availableOptionChosen = true;
                         break;
                     case "2":
-                        newHero.HeroClass = "Mage";
+                        newHero.Class = "Mage";
                         availableOptionChosen = true;
                         break;
                     case "3":
-                        newHero.HeroClass = "Rogue";
+                        newHero.Class = "Rogue";
                         availableOptionChosen = true;
                         break;
                     default:
@@ -45,72 +43,61 @@
         }
     }
 
-    interface ICharacter
+    interface IHero
     {
-        int CharacterMaxHealth { get; set; }
-        int CharacterStrength { get; set; }
-        int CharacterIntelligence { get; set; }
-        int CharacterAgility { get; set; }
-        int CharacterStamina { get; set; }
+        int MaxHealth { get; set; }
+        int Strength { get; set; }
+        int Intelligence { get; set; }
+        int Agility { get; set; }
+        int Stamina { get; set; }
     }
 
-    class Hero : ICharacter
+    class Hero : IHero
     {
-        public string HeroName;
-        public string HeroClass;
+        public string Class;
         private int Level, Experience, Gold;
-
-        public void Character(int characterMaxHealth, int characterStrength, int characterIntelligence, int characterAgility, int characterStamina )
-        {
-            CharacterMaxHealth = characterMaxHealth;
-            CharacterStrength = characterStrength;
-            CharacterIntelligence = characterIntelligence;
-            CharacterAgility = characterAgility;
-            CharacterStamina = characterStamina;
-        }
-        
-        public int CharacterMaxHealth { get; set; }
-        public int CharacterStrength { get; set; }
-        public int CharacterIntelligence { get; set; }
-        public int CharacterAgility { get; set; }
-        public int CharacterStamina { get; set; }
+        public int MaxHealth { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Agility { get; set; }
+        public int Stamina { get; set; }
 
         public static void ClassSelect(Hero newHero)
         {
-            newHero.CharacterStrength = 10;
-            newHero.CharacterAgility = 10;
-            newHero.CharacterIntelligence = 10;
-            newHero.CharacterMaxHealth = 100;
-            newHero.CharacterStamina = 100;
+            newHero.Strength = 10;
+            newHero.Agility = 10;
+            newHero.Intelligence = 10;
+            newHero.MaxHealth = 100;
+            newHero.Stamina = 100;
             newHero.Level = 1;
             newHero.Experience = 0;
             newHero.Gold = 0;
            
 
-            switch (newHero.HeroClass)
+            switch (newHero.Class)
             {
                 case "Warrior":
-                    newHero.CharacterMaxHealth = newHero.CharacterMaxHealth * 115 / 100;
-                    newHero.CharacterStrength = newHero.CharacterStrength * 200 / 100;
+                    newHero.MaxHealth = newHero.MaxHealth * 115 / 100;
+                    newHero.Strength = newHero.Strength * 200 / 100;
                     break;
                 case "Mage":
-                    newHero.CharacterIntelligence = newHero.CharacterIntelligence * 160 / 100;
+                    newHero.MaxHealth = newHero.MaxHealth * 20 / 1000;
+                    newHero.Intelligence = newHero.Intelligence * 160 / 100;
                     break;
                 case "Rogue":
-                    newHero.CharacterAgility = newHero.CharacterAgility * 110 / 100;
+                    newHero.Agility = newHero.Agility * 110 / 100;
                     break;
             }
-
-            Console.WriteLine($"Your name is {newHero.HeroName}");
-            Console.WriteLine($"Your class is {newHero.HeroClass}");
+            
+            Console.WriteLine($"Your class is {newHero.Class}");
             Console.WriteLine($"Your Level is {newHero.Level}");
             Console.WriteLine($"Your Experience is {newHero.Experience}");
             Console.WriteLine($"Your Gold is {newHero.Gold}");
-            Console.WriteLine($"Your Health is {newHero.CharacterMaxHealth}");
-            Console.WriteLine($"Your Strength is {newHero.CharacterStrength}");
-            Console.WriteLine($"Your Intelligence is {newHero.CharacterIntelligence}");
-            Console.WriteLine($"Your Agility is {newHero.CharacterAgility}");
-            Console.WriteLine($"Your Stamina is {newHero.CharacterStamina}");
+            Console.WriteLine($"Your Health is {newHero.MaxHealth}");
+            Console.WriteLine($"Your Strength is {newHero.Strength}");
+            Console.WriteLine($"Your Intelligence is {newHero.Intelligence}");
+            Console.WriteLine($"Your Agility is {newHero.Agility}");
+            Console.WriteLine($"Your Stamina is {newHero.Stamina}");
         }
     }
 }
