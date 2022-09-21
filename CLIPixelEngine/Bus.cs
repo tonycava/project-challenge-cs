608ed660a7587b384b703ea4f73df524c132773c
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-21 09:02:38 +0200

    fix: resize map

--- CLIPixelEngine/Bus.cs
+++ CLIPixelEngine/Bus.cs
@@ -3,48 +3,45 @@ using System.Collections.Generic;
 
 namespace CLIPixelEngine.Engine.Bus
 {
-    
-    public enum Actions
-    {
-        //input
-        BUTTON_PRESS,
-        
-        //movement
-        COLLISION,
-        MOVEMENT,
-        
-        //scene
-        CHANGE_MAP,
-        
-        
-    }
-    
-    public enum ActionType
-    {
-        INPUT,
-        RENDER,
-        SCENE,
-    }
-    
-    
-    public class MessageBus
-    {
-        //TODO: Fill with enum
-        
-        public Queue<Message> Mqueue;
-    }
-    
-    public class Message
-    {
-        public ActionType type;
-        public Actions action;
-        public string value;
+  public enum Actions
+  {
+    //input
+    BUTTON_PRESS,
 
-        public Message(ActionType t,Actions a,string v)
-        {
-            this.type = t;
-            this.action = a;
-            this.value = v;
-        }
+    //movement
+    COLLISION,
+    MOVEMENT,
+
+    //scene
+    CHANGE_MAP,
+  }
+
+  public enum ActionType
+  {
+    INPUT,
+    RENDER,
+    SCENE,
+  }
+
+
+  public class MessageBus
+  {
+    //TODO: Fill with enum
+
+    public Queue<Message> Mqueue;
+  }
+
+  public class Message
+  {
+    public ActionType Type;
+    public Actions Action;
+    public string Value;
+
+    public Message(ActionType type, Actions action, string value)
+    {
+      Type = type;
+      Action = action;
+      Value = value;
     }
+  }
 }
--- CLIPixelEngine/Renderer.cs
+++ CLIPixelEngine/Renderer.cs
@@ -5,63 +5,77 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using CLIPixelEngine.Engine.Bus;
 using CLIPixelEngine.Engine.Generic;
-using Color = CLIPixelEngine.Engine.Generic.Color;
+using Game.EntityHandler.Maps.DebugMAP;
 
-namespace CLIPixelEngine.Engine {
-	public class Renderer {
+namespace CLIPixelEngine.Engine
+{
+  public class Renderer
+  {
+    public static IntPtr handle;
 
-		public static IntPtr handle;
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int mode);
 
-		[DllImport("kernel32.dll", SetLastError = true)]
-		public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int mode);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool GetConsoleMode(IntPtr handle, out int mode);
 
-		[DllImport("kernel32.dll", SetLastError = true)]
-		public static extern bool GetConsoleMode(IntPtr handle, out int mode);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr GetStdHandle(int handle);
 
-		[DllImport("kernel32.dll", SetLastError = true)]
-		public static extern IntPtr GetStdHandle(int handle);
-		
-		
-		private static void SetupConsole()
-		{
-			handle = GetStdHandle(-11);
-			int mode;
-			GetConsoleMode(handle, out mode);
-			SetConsoleMode(handle, mode | 0x4);
-		}
 
-		static Bitmap GetMap()
-		{
-	    	//TODO: return selected map
-			return new Bitmap("./Assets/map.png");
-		}
+    private static void SetupConsole()
+    {
+      handle = GetStdHandle(-11);
+      int mode;
+      GetConsoleMode(handle, out mode);
+      SetConsoleMode(handle, mode | 0x4);
+    }
 
-		public static void Draw()
-		{
-			//TODO: GET MAP
-			Bitmap Map = GetMap();
+    static Bitmap GetMap(string pathToMap)
+    {
+      return new Bitmap(pathToMap);
+    }
 
-			if(handle == IntPtr.Zero)SetupConsole();
+    public static void Draw()
+    {
+      const int fov = 5;
+      Vector2Int camera = Camera.cordOfCharacter;
+      Vector2Int sizeOfMap = DebugMap.sizeOfMap;
 
-			for (int x = 0; x < Map.Width;	 x++)
-			{
-				for (int y = 0; y < Map.Height; y++)
-				{
-					byte r = Map.GetPixel(y, x).R;
-					byte g = Map.GetPixel(y, x).G;
-					byte b = Map.GetPixel(y, x).B;
 
-					Console.Write("\x1b[48;2;" + r + ";" + g + ";" + b + "m  ");
-				}
+      Bitmap Map = GetMap("./Assets/Maps/DebugMAP/DebugMAP.png");
 
-				Console.Write("\x1b[48;2;" + 0 + ";" + 0 + ";" + 0 + "m");
-				Console.Write("\n");
-			}
-		}
+      if (handle == IntPtr.Zero) SetupConsole();
 
-		public void SetMap(string path)
-		{
-			
-		}
-	}
+
+      int StartAtX = camera.x - fov < 0 ? 0 : camera.x - fov;
+      
+      int EndAtX = camera.x + sizeOfMap.x > sizeOfMap.x ? sizeOfMap.x : camera.x + sizeOfMap.x;      
+      
+      
+      int StartAtY = camera.y - fov < 0 ? 0 : camera.y - fov;
+      
+      int EndAtY = camera.y + sizeOfMap.y > sizeOfMap.y ? sizeOfMap.y : camera.y + sizeOfMap.y;
+
+      
+      for (int x = StartAtX; x < EndAtX; x++)
+      {
+        for (int y = StartAtY; y < EndAtY; y++)
+        {
+          byte r = Map.GetPixel(y, x).R;
+          byte g = Map.GetPixel(y, x).G;
+          byte b = Map.GetPixel(y, x).B;
+
+          Console.Write("\x1b[48;2;" + r + ";" + g + ";" + b + "m  ");
+        }
+
+        Console.Write("\x1b[48;2;" + 0 + ";" + 0 + ";" + 0 + "m");
+        Console.Write("\n");
+      }
+    }
+
+    public void SetMap(string path)
+    {
+    }
+  }
 }
